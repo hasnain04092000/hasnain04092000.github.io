989ce6658f63f7bb53ff1e975053b3296cca9a08
hasnain04092000 <66196007+hasnain04092000@users.noreply.github.com>
2020-11-30 15:25:50 +0600

    upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function one(){
+ document.getElementById("one").style.display="none";
+ document.getElementById("two").style.display="block";
+}
+function two(){
+ document.getElementById("one").style.display="block";
+ document.getElementById("two").style.display="none";
+}
